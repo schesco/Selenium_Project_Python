940d761d4ec1917ba2aa2d73372aab5b601d6f52
ngbwa <francispascal@hotmail.de>
2026-04-03 21:43:33 +0200

    Project selenium refresh3

--- Utilities/BaseClass.py
+++ Utilities/BaseClass.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,9 +11,21 @@ class BaseClass:
 
     @pytest.fixture
     def setup(self):
+        chrome_options = Options()
 
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
-        driver.maximize_window()
+        if os.getenv("CI"):
+            chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--window-size=1920,1080")
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+            driver.maximize_window()
+        else:
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service)
+            driver.maximize_window()
+
         yield driver
         driver.quit()
