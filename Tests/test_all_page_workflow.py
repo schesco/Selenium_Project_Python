177ca1f8c4fba5a12f41c8c2a64ce980bb57ef3e
ngbwa <francispascal@hotmail.de>
2026-04-03 22:55:14 +0200

    Project add testallworkflow

--- Tests/test_all_page_workflow.py
+++ Tests/test_all_page_workflow.py
@@ -9,7 +9,7 @@ from Utilities.BaseClass import BaseClass
 from Utilities.Expected import expect
 from Tests.test_shop_page import TestShopPage
 
-class TestHomePage(BaseClass):
+class TestAllPageWorkflow(BaseClass):
 
     def testHomepageform(self, setup):
         driver = setup
@@ -46,39 +46,44 @@ class TestHomePage(BaseClass):
 
 
 
+        Shop = ShopPage(driver)
+        Shop.set_shop_view()
+        Shop.Choose_Product("iphone X")
+        Shop.Choose_Product("Blackberry")
+        Shop.Choose_Product("Samsung Note 8")
+        Shop.Choose_Product("Nokia Edge")
+        Shop.CheckOut()
 
 
+        ConfirmShop = ConfirmPage(driver)
+        ConfirmShop.set_product_number("iphone X",5)
+        ConfirmShop.set_product_number("Nokia Edge",1)
+        ConfirmShop.set_product_number("Samsung Note 8",2)
+        ConfirmShop.set_product_number("Blackberry", 4)
+        ConfirmShop.give_price_total()
+        ConfirmShop.Check_out()
 
 
+        PurchasePage_object=PurchasePage(driver)
+        PurchasePage_object.setCountry()
+        PurchasePage_object.selectCountry()
+        PurchasePage_object.purchaseProduct()
+        PurchasePage_object.policy()
+        PurchasePage_object.readResult_state()
+        sleep(5)
 
 
+        driver.get("https://rahulshettyacademy.com/client/")
 
+        page = RegisterPage(driver)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       # obj=TestShopPage()
-        #obj.testShopInput(setup)
+        page.open_register()
+        page.fill_form("francis", "ngbwa", "francispascal@hotmail.de", "6777898878")
+        page.select_occupation("Engineer")
+        page.select_gender()
+        page.set_password("Fr@ncis1983")
+        page.accept_terms()
+        page.submit_form()
 
 
 
--- Utilities/BaseClass.py
+++ Utilities/BaseClass.py
@@ -13,19 +13,17 @@ class BaseClass:
     def setup(self):
         chrome_options = Options()
 
+        # Headless nur im CI aktivieren
         if os.getenv("CI"):
             chrome_options.add_argument("--headless=new")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--window-size=1920,1080")
-            service = Service(ChromeDriverManager().install())
-            driver = webdriver.Chrome(service=service, options=chrome_options)
-            driver.maximize_window()
-        else:
-            service = Service(ChromeDriverManager().install())
-            driver = webdriver.Chrome(service=service)
-            driver.maximize_window()
 
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+
+        driver.implicitly_wait(5)
         yield driver
         driver.quit()
